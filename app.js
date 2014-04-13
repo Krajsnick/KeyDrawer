8023138710b85ff5a36f9c6b70e925e1dd5e436e
Robert Ahlberg <rob.ahlberg@gmail.com>
2014-04-13 13:20:46 +0200

    Use node as backend add socket.io support

---
+++ app.js
@@ -0,0 +1,32 @@
+var fs = require('fs')
+, http = require('http')
+, io = require('socket.io');
+
+var server = http.createServer(function (req, res) {
+  console.log("Request for: " + req.url);
+
+  var filePath = '.' + req.url;
+  if (filePath == './') filePath = './drawer.html';
+
+  fs.exists(filePath, function(exists) {
+    if (exists) {
+      fs.readFile(filePath, function(error, content) {
+        if (error) {
+          res.writeHead(500);
+          res.end();
+        } else {
+          res.writeHead(200);
+          res.end(content, 'utf-8');
+        }
+      });
+    } else {
+      res.writeHead(404);
+      res.end();
+    }
+  });
+
+  // res.writeHead(200, {'Content-Type': 'text/html'});
+  // res.end(fs.readFileSync(__dirname + '/drawer.html'));
+}).listen(3000, function(){
+  console.log("Server running at 3000");
+});
